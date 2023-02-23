5c3272434cfcba63b2ee9d65253db9432a33aa52
ShaneIan <shaneyfelt.ian@gmail.com>
2023-02-23 12:22:39 -0600

    DataManager

--- JavaScheduleBuilder/src/CourseDataManager/DataManager.java
+++ JavaScheduleBuilder/src/CourseDataManager/DataManager.java
@@ -1,5 +1,26 @@
 package CourseDataManager;
 
+
+
 public class DataManager {
     
+    //client sends lists of demands for scheudler
+    //create a sql query from those and return data
+    public void buildQuerryFromSearch(){
+
+    }
+    //creates the query
+    public void querySQLServer(){
+
+    }
+    //take sql querrys to fill out hashmap 
+    //for any given students schedule request
+    public void buildCourseObjFromQuery(){
+
+    }
+    //given user query creates a matching set of schedules
+    //that fit some given set of query characteristics
+    public void compileSchedulesFromQuery(){
+
+    }
 }
