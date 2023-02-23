217e9482adca1fc5ece527c752850c5e3df51a80
Raedans21 <raedans21@gmail.com>
2023-02-22 19:45:48 -0600

    Started implementing schedule class

--- JavaScheduleBuilder/src/ScheduleObjectBuilder/Schedule.java
+++ JavaScheduleBuilder/src/ScheduleObjectBuilder/Schedule.java
@@ -1,5 +1,35 @@
 package ScheduleObjectBuilder;
+import CourseObjectBuilder.*;
+import java.util.*;
 
 public class Schedule {
-    
+    private Course[] courses;
+    private int totalHours;
+    private HashMap<String, String> classTimes;
+
+    public Schedule(Course[] courses) {
+        courses = this.courses;
+        totalHours = calculateTotalHours();
+        classTimes = compileClassTimes();
+    }
+
+    public int calculateTotalHours() {
+        int numScheduleHours = 0;
+        for (Course course: courses) {
+            numScheduleHours += course.getCreditHours();
+        }
+        return numScheduleHours;
+    }
+
+    public HashMap<String, String> compileClassTimes() {
+        HashMap<String, String> classTimeAndDayMap = new HashMap<String, String>();
+        String dayAndTimeStr;
+        for (Course course: courses) {
+            dayAndTimeStr = course.getClassDays() + " " + course.getClassTime();
+            classTimes.put(course.getCourseTitle(), dayAndTimeStr);
+        }
+        return classTimeAndDayMap;
+    }
+
+
 }
