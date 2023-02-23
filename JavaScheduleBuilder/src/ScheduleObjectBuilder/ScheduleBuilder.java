efc01e2730dacfccd2583fc8244f0093a2ec462d
Juneau789 <Hjunea9@gmail.com>
2023-02-22 20:17:39 -0600

    added variables to schedule builder

--- JavaScheduleBuilder/src/ScheduleObjectBuilder/ScheduleBuilder.java
+++ JavaScheduleBuilder/src/ScheduleObjectBuilder/ScheduleBuilder.java
@@ -1,5 +1,15 @@
 package ScheduleObjectBuilder;
 
+import java.util.*; 
+import CourseObjectBuilder.*;
+
 public class ScheduleBuilder {
+        private HashMap<String, Course[]> courses;
+        private ConflictResolver conflictResolver;
+        private Schedule schedule;
+        
+        
+        
+           
     
 }
