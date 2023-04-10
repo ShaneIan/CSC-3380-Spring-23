4b309be31493e0c3b01b9193629ee5f87a2152f8
ShaneIan <shaneyfelt.ian@gmail.com>
2023-04-10 15:56:51 -0500

    fixed small issue

--- JavaScheduleBuilder/src/ScheduleAnalyzer/ScheduleSpreadAnalyzer.java
+++ JavaScheduleBuilder/src/ScheduleAnalyzer/ScheduleSpreadAnalyzer.java
@@ -66,6 +66,6 @@ public class ScheduleSpreadAnalyzer implements ScheduleAnalyzer{
         double rankingpoints = schedule.getNumberHoursOfGap(); 
         return rankingpoints;     
     }
-    
-}
 */
+}
+
