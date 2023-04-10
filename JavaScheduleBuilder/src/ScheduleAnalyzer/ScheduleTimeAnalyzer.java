147cc0821b9306dd9deaa32a5270bb9b9c188ce6
WilliamDuke02 <wduke2002@gmail.com>
2023-04-10 09:21:44 -0500

    fixed up timeAnalyzer.

--- JavaScheduleBuilder/src/ScheduleAnalyzer/ScheduleTimeAnalyzer.java
+++ JavaScheduleBuilder/src/ScheduleAnalyzer/ScheduleTimeAnalyzer.java
@@ -1,18 +1,19 @@
 package ScheduleAnalyzer;
 import ScheduleObjectBuilder.*;
+import java.util.*;
 //Class specifically for analyzing the timing of schedules to  
 public class ScheduleTimeAnalyzer implements ScheduleAnalyzer{
     private Schedule[] schedules;
-    HashMap<int, Schedule> schedulesHashMap;
+    HashMap<Integer, Schedule> schedulesHashMap = new HashMap<Integer, Schedule>();
     int[] sortedSchedulesArray;
 
 
     public ScheduleTimeAnalyzer(Schedule[] schedules) {
         schedules = this.schedules;
-        sortedSchedulesArray = rankScheduleOptions();
         for(int i = 0; i < schedules.length; i++){  //key:original index in schedules value: schedule 
             schedulesHashMap.put(i, schedules[i]);
         }
+        rankScheduleOptions(); //sets sortedSchedulesArray
     }
 
     @Override
@@ -23,44 +24,44 @@ public class ScheduleTimeAnalyzer implements ScheduleAnalyzer{
 
     @Override
 
-    public int[] rankScheduleOptions() {  //returns a sorted array of key values for schedulesHashMap based on morning rank
+    public void rankScheduleOptions() {  //returns a sorted array of key values for schedulesHashMap based on morning rank
         
         //throw new UnsupportedOperationException("Unimplemented method 'rateSchedule'");
         int[] arrayOfKeys = new int[schedules.length];
+        double[] arrayOfRanks = new double[schedules.length];
         for (int i = 0; i < schedules.length; i++){
-            arrayOfKeys[i] = i
+            arrayOfKeys[i] = i;
+            arrayOfRanks[i] = ScheduleRanker(schedules[i]); //this could be schedules[i].getNumberHoursBeforeTwelve
+
         }
-        int[] arrayOfRanks = new int[schedules.length];
-        for (int i = 0; i < schedules.length; i++){
-            arrayOfRanks[i] = ScheduleRanker(schedules[i]) //this could be schedules[i].getNumberHoursBeforeTwelve
-        }
+        
         int n = schedules.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arrayOfRanks[j] < arrayOfRanks[j + 1]) {
-                    int temp = arrayOfRanks[j];
+                    double tempR = arrayOfRanks[j];
                     arrayOfRanks[j] = arrayOfRanks[j + 1];
-                    arrayOfRanks[j + 1] = temp;
-                    int temp = arrayOfKeys[j];
+                    arrayOfRanks[j + 1] = tempR;
+                    int tempK = arrayOfKeys[j];
                     arrayOfKeys[j] = arrayOfKeys[j + 1];
-                    arr[j + 1] = temp;
+                    arrayOfKeys[j + 1] = tempK;
                 }
             }
         }
-        return arrayOfKeys;
+        sortedSchedulesArray = arrayOfKeys;
     }
 
     @Override
     public void getHighestRankedScheduleOption() { //returns schedule with most amount of morning hours 
-        //throw new UnsupportedOperationException("Unimplemented method 'rateSchedule'");
-        return schedulesHashMap.get(sortedSchedulesArray[0]);
+        throw new UnsupportedOperationException("Unimplemented method 'rateSchedule'");
+        //return schedulesHashMap.get(sortedSchedulesArray[0]);
     }
 
     @Override
     public void getLowestRankedScheduleOption() { //returns schedule with the least amount of hours before 12
         
-        //throw new UnsupportedOperationException("Unimplemented method 'rateSchedule'");
-        return schedulesHashMap.get(sortedSchedulesArray[sortedSchedulesArray.length - 1]);
+        throw new UnsupportedOperationException("Unimplemented method 'rateSchedule'");
+        //return schedulesHashMap.get(sortedSchedulesArray[sortedSchedulesArray.length - 1]);
     }
 
     public double ScheduleRanker(Schedule schedule) {  //returns total number of hours before 12 for a certain schedule
